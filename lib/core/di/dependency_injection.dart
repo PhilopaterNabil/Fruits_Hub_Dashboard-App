0014b8c6793d0aca9788346004bc6242e5b1145b
Philopater Nabil <fylwbatyrnbylnasr@gmail.com>
2025-07-27 22:35:25 +0300

    feat: Implement Supabase storage service; update dependency injection and file upload logic

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -3,9 +3,9 @@ import 'package:fruits_hub_dashboard/core/repos/images_repo/images_repo_impl.dar
 import 'package:fruits_hub_dashboard/core/repos/product_repo/products_repo.dart';
 import 'package:fruits_hub_dashboard/core/repos/product_repo/products_repo_impl.dart';
 import 'package:fruits_hub_dashboard/core/services/database_service.dart';
-import 'package:fruits_hub_dashboard/core/services/fire_storage.dart';
 import 'package:fruits_hub_dashboard/core/services/firestore_service.dart';
 import 'package:fruits_hub_dashboard/core/services/storage_service.dart';
+import 'package:fruits_hub_dashboard/core/services/supabase_storage_service.dart';
 import 'package:get_it/get_it.dart';
 
 final getIt = GetIt.instance;
@@ -15,8 +15,11 @@ Future<void> setupGetIt() async {
   // final dio = await DioFactory.getDio();
   // getIt.registerLazySingleton<ApiService>(() => ApiService(dio));
 
-  // firebase storage service
-  getIt.registerLazySingleton<StorageService>(() => FireStorage());
+  // // firebase storage service
+  // getIt.registerLazySingleton<StorageService>(() => FireStorage());
+
+  // supabase storage service
+  getIt.registerLazySingleton<StorageService>(() => SupabaseStorageService());
   // database service
   getIt.registerLazySingleton<DatabaseService>(() => FirestoreService());
 
--- lib/core/services/supabase_storage_service.dart
+++ lib/core/services/supabase_storage_service.dart
@@ -2,11 +2,12 @@ import 'dart:io';
 
 import 'package:fruits_hub_dashboard/core/services/storage_service.dart';
 import 'package:fruits_hub_dashboard/core/utils/constants.dart';
+import 'package:path/path.dart' as b;
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SupabaseStorageService implements StorageService {
   static late Supabase _supabase;
-  static  initSupabase() async {
+  static initSupabase() async {
     _supabase = await Supabase.initialize(
       url: kSupabaseUrl,
       anonKey: kSupabaseAnonKey,
@@ -14,8 +15,13 @@ class SupabaseStorageService implements StorageService {
   }
 
   @override
-  Future<String> uploadFile(File file, String path) {
-    // TODO: implement uploadFile
-    throw UnimplementedError();
+  Future<String> uploadFile(File file, String path) async {
+    String fileName = b.basename(file.path);
+    String extension = b.extension(file.path);
+    var result = await _supabase.client.storage
+        .from('fruits_images')
+        .upload('$path/$fileName.$extension', file);
+
+    return result;
   }
 }
